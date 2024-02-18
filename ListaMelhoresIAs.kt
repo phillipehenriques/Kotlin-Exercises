187838b6111d6e1fb14c1712103a6c1697a522ca
Phillipe Csorgo Henriques <106546716+phillipehenriques@users.noreply.github.com>
2024-02-18 19:43:47 -0300

    mfc

---
+++ ListaMelhoresIAs.kt
@@ -0,0 +1,21 @@
+fun main() {
+    println("Saiba as 10 principais IA's da atualidade!")
+    println("Lista das 10 melhores")
+
+    val InteligenciasArtificiais = listOf(
+        "1. OpenAI GPT-3",
+        "2. Copilot",
+        "3. Google Assistant",
+        "4. Amazon Alexa",
+        "5. Microsoft Cortana",
+        "6. Lexica",
+        "7. Tesla Autopilot",
+        "8. Facebook DeepFace",
+        "9. Mid Journey",
+        "10. Gemini Pro"
+    )
+
+    for (ia in InteligenciasArtificiais) {
+        println(ia)
+    }
+}
